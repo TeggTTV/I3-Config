9b7e9629fc7f38f4958bab9d901f1ad5225a662b
TeggTTV <doe034945@gmail.com>
2024-07-09 11:14:24 -0400

    NeoVim

---
+++ .config/nvim/lua/teggundrut/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/Bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = { mode = "tabs", seperator_style = "slant", }
+  }
+} 
